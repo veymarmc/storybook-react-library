61d295bc6f7a0f27de97dab9fa49703553479845
veymarmc <veymar.montano@gmail.com>
2022-05-31 18:40:23 -0400

    order the components folder

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-import { Button } from './Button';
+import { Button } from '../button/Button';
 import './header.scss';
 
 export const Header = ({ user, onLogin, onLogout, onCreateAccount }) => (
