1e06e8fe16274bd897e4ae6c099dbde783b6ea25
veymarmc <veymar.montano@gmail.com>
2022-06-05 16:12:43 -0400

    add accordion type declarations

--- src/components/declarationFiles/d.ts/Accordion.d.ts
+++ src/components/declarationFiles/d.ts/Accordion.d.ts
@@ -1,7 +1,21 @@
-import { FunctionComponent } from 'react';
+import React, { FunctionComponent } from 'react';
+
+interface AccordionItemProps {
+  title: string,
+  content: React.ReactNode,
+  active?: boolean
+}
 
 interface AccordionProps {
-
+  data?: Arra<AccordionItemProps>;
+  primary?: boolean,
+  secondary?: boolean,
+  inverted?: boolean,
+  expandIcon?: string,
+  shrinkIcon?: string,
+  backgrounColor?: string,
+  color?: string,
+  className?: string,
 }
 
 export const Accordion: FunctionComponent<AccordionProps>;
