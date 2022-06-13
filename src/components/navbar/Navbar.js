f15789ea3a1faf0111a0925ab682684aa428f374
veymarmc <veymar.montano@gmail.com>
2022-06-13 08:18:52 -0400

    add navbar basic structure
    - create styles for align-items property
    - create a default navbar with only the tile
    - create a navbar with actions

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -2,18 +2,35 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import './navbar.scss';
 
-export const Navbar = (props) => {
+export const Navbar = ({ title, actions }) => {
   return (
-    <div>
-      navbar
+    <div className='vmcrjc-navbar navbar'>
+      <h2>{title}</h2>
+      
+      {!!actions.length &&
+      <div className='navbar__actions'>
+        {actions.map((a, i) => (
+          <React.Fragment key={i}>
+            {a}
+          </React.Fragment>
+        ))}
+      </div>
+      }
     </div>
   );
 }
 
 Navbar.propTypes = {
-  
+  /**
+   * actions in the navbar that can be any react node.
+   */
+  actions: PropTypes.arrayOf(PropTypes.node),
+  /**
+   * title of the navbar, usually application name
+   */
+  title: PropTypes.string,
 };
 
 Navbar.defaultProps = {
-
+  actions: [],  
 };
--- src/stories/Navbar.stories.js
+++ src/stories/Navbar.stories.js
@@ -1,11 +1,26 @@
 import React from 'react';
-import { Navbar } from '../components';
+import { Navbar, Button } from '../components';
+import { Primary, Secondary } from './Button.stories';
 
 export default {
   title: 'Example/Navbar',
   component: Navbar
 };
 
+const actions = [
+  <Button {...Primary.args} />,
+  <Button {...Secondary.args} />,
+]
+
 const Template = (args) => <Navbar {...args} />;
 
 export const Default = Template.bind({});
+Default.args = {
+  title: 'Navbar title',
+}
+
+export const Actions = Template.bind({});
+Actions.args = {
+  ...Default.args,
+  actions
+};
